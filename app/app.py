a424265f1c65312329657e00a444727fbfa79a54
柳橋莉空 <yagihashirikuu@yagihashirikuunoMacBook-Air.local>
2023-11-20 16:41:49 +0900

    Add: Login feature

--- app/app.py
+++ app/app.py
@@ -1,6 +1,9 @@
 import os
+from datetime import datetime
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
+from flask_login import (LoginManager, UserMixin, login_required, login_user,
+                         logout_user)
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
@@ -13,9 +16,45 @@ app.config[
     host=os.environ.get("POSTGRES_HOST"),
     port=os.environ.get("POSTGRES_PORT"),
     db=os.environ.get("POSTGRES_DB"),
+
 )
+app.config['SECRET_KEY'] = 'your_secret_key_here'
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    user = db.relationship("User", backref=db.backref("posts", lazy=True))
+
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    image_url = db.Column(db.String(255), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id"), nullable=False)
+    post = db.relationship("Post", backref=db.backref("images", lazy=True))
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 @app.route("/")
@@ -23,9 +62,53 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/create")
+@app.route("/create", methods=["GET", "POST"])
 def create():
+    if request.method == "POST":
+        title = request.form.get("title")
+        content = request.form.get("content")
+        post = Post(title=title, content=content)
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for("index"))
     return render_template("create.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:  # 通常はパスワードをハッシュ化して比較します
+            login_user(user)
+            return redirect(url_for("index"))
+        else:
+            return "Invalid username or password"
+    return render_template("login.html")
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = User.query.filter_by(username=username).first()
+        if user:
+            return "Username already exists"
+        new_user = User(username=username, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for("login"))
+    return render_template("signup.html")
 
 
 if __name__ == "__main__":
