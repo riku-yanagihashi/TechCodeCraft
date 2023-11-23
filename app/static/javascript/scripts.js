a11d20e00ba23ed2c7de6e393e6b52b586d06d2f
柳橋莉空 <yagihashirikuu@yagihashirikuunoMacBook-Air.local>
2023-11-24 03:10:37 +0900

    Add: header button hover animation left to right

--- app/static/javascript/scripts.js
+++ app/static/javascript/scripts.js
@@ -1,11 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-	document.querySelector('a[href="#top"]').addEventListener('click', function(e) {
-		e.preventDefault();
 
-		// スムーズなスクロールのためのアニメーション
-		document.querySelector('html, body').animate({
-			scrollTop: 0
-		}, 800); // アニメーションの速さ（ミリ秒単位）
-
-	});
-});
